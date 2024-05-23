229ede85e7618bb526fb80ac6a26b774cd0212b1
Matheusbcy <matheusbcy@gmail.com>
2024-05-23 09:09:34 -0300

    endpoint login and error handling

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,43 +1,74 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
 import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
+import { BadRequestsException } from "../exceptions/bad-request";
+import { ErrorCode } from "../exceptions/root";
 
-export const signup = async (req: Request, res: Response) => {
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { nome, email, password } = req.body;
+  const saltsRonds = 8;
+  const hashedPassword = bcrypt.hashSync(password, saltsRonds);
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (user) {
+    return next(
+      new BadRequestsException(
+        "User already exists!",
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    );
+  }
+  user = await prismaClient.user.create({
+    data: {
+      nome,
+      email,
+      password: hashedPassword,
+    },
+  });
+  res.status(201).json(user);
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const { nome, email, password } = req.body;
-
-    if (!nome || !email || !password) {
-      return res
-        .status(400)
-        .json({ error: "Name, email, and password are required." });
-    }
-
-    const saltsRonds = 8;
-
-    if (typeof password !== "string" || password.trim() === "") {
-      return res.status(400).json({
-        error: "Password is required and must be a non-empty string.",
-      });
-    }
-
-    const hashedPassword = bcrypt.hashSync(password, saltsRonds);
+    const { email, password } = req.body;
 
     let user = await prismaClient.user.findFirst({ where: { email } });
 
-    if (user) {
-      throw new Error("User already exists!");
+    if (!user) {
+      return next(
+        new BadRequestsException(
+          "User does not exists",
+          ErrorCode.USER_NOT_FOUND
+        )
+      );
     }
-
-    user = await prismaClient.user.create({
-      data: {
-        nome,
-        email,
-        password: hashedPassword,
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(
+        new BadRequestsException(
+          "Incorrect password",
+          ErrorCode.INCORRECT_PASSWORD
+        )
+      );
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
       },
-    });
+      JWT_SECRET
+    );
 
-    res.status(201).json(user);
+    res.json({ user, token });
   } catch (error) {
-    res.status(500).json(error);
+    res.send(error);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
 
-app.use(express.json())
+app.use(express.json());
+
+// Rotas principais
+app.use("/api", rootRouter);
+
+// Middleware de tratamento de erros
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log("App working!");
 });
 
 export const prismaClient = new PrismaClient({
-    log:['query']
-})
-
-app.use("/api", rootRouter)
+  log: ["query"],
+});
