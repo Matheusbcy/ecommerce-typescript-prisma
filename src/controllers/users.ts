edd5a448dff8bc7f4f3b347941544cf394d1cb23
Matheusbcy <matheusbcy@gmail.com>
2024-06-03 20:28:53 -0300

    updated users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
-import { AddressSchema } from "../schema/users";
+import { AddressSchema, UpdateUserSchema } from "../schema/users";
 import { prismaClient } from "..";
 import { NotFound } from "../exceptions/not-found";
 import { ErrorCode } from "../exceptions/root";
+import { Address } from "@prisma/client";
+import { BadRequestsException } from "../exceptions/bad-request";
 
 export const addAddress = async (req: Request, res: Response) => {
   AddressSchema.parse(req.body);
@@ -34,4 +36,63 @@ export const listAddress = async (req: Request, res: Response) => {
     },
   });
   res.json(address);
+};
+
+export const updateUsers = async (req: Request, res: Response) => {
+  const validatedData = UpdateUserSchema.parse(req.body);
+  let shippingAddress: Address;
+  let billingAddress: Address;
+
+  if (validatedData.defaultShippingAddress) {
+    try {
+      shippingAddress = await prismaClient.address.findFirstOrThrow({
+        where: {
+          id: validatedData.defaultShippingAddress,
+        },
+      });
+    } catch (error) {
+      throw new NotFound(
+        "Address not found",
+        ErrorCode.ADDRESS_NOT_FOUND,
+        null
+      );
+    }
+    if (shippingAddress.userId != req.user.id) {
+      throw new BadRequestsException(
+        "Address does not belong to user",
+        ErrorCode.ADDRESS_DOES_NOT_BELONG
+      );
+    }
+  }
+
+  if (validatedData.defaultBillingAddress) {
+    try {
+      billingAddress = await prismaClient.address.findFirstOrThrow({
+        where: {
+          id: validatedData.defaultBillingAddress,
+        },
+      });
+    } catch (error) {
+      throw new NotFound(
+        "Address not found",
+        ErrorCode.ADDRESS_NOT_FOUND,
+        null
+      );
+    }
+    if (billingAddress.userId != req.user.id) {
+      throw new BadRequestsException(
+        "Address does not belong to user",
+        ErrorCode.ADDRESS_DOES_NOT_BELONG
+      );
+    }
+  }
+
+  const updatedUser = await prismaClient.user.update({
+    where: {
+      id: req.user.id,
+    },
+    data: validatedData,
+  });
+
+  res.json(updatedUser);
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,12 +2,22 @@ import { Router } from "express";
 import authMiddleware from "../middlewares/auth";
 import adminMiddleWare from "../middlewares/admin";
 import { errorHandle } from "../error-handler";
-import { addAddress, deleteAddress, listAddress } from "../controllers/users";
+import {
+  addAddress,
+  deleteAddress,
+  listAddress,
+  updateUsers,
+} from "../controllers/users";
 
-const usersRoutes: Router = Router()
+const usersRoutes: Router = Router();
 
-usersRoutes.post("/address", [authMiddleware], errorHandle(addAddress))
-usersRoutes.delete("/address/:id", [authMiddleware], errorHandle(deleteAddress))
-usersRoutes.get("/address", [authMiddleware], errorHandle(listAddress))
+usersRoutes.post("/address", [authMiddleware], errorHandle(addAddress));
+usersRoutes.delete(
+  "/address/:id",
+  [authMiddleware],
+  errorHandle(deleteAddress)
+);
+usersRoutes.get("/address", [authMiddleware], errorHandle(listAddress));
+usersRoutes.put("/", [authMiddleware], errorHandle(updateUsers));
 
-export default usersRoutes
+export default usersRoutes;
